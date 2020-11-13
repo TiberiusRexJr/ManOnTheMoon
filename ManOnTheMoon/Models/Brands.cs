f64d7be021f9b4ee37568977e014eeb16d54f16e
Lexus <rounin_lopez@yahoo.com>
2020-11-12 20:56:05 -0600

    added tables to proejct database. working on models with linq added database code all in progress

---
+++ ManOnTheMoon/Models/Brands.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManOnTheMoon.Models
+{
+    public class Brands
+    {
+        [Association(Name = "FK_Orders_Customers", Storage = "_Customer", ThisKey = "CustomerID", IsForeignKey = true)]
+    }
+}
---
+++ ManOnTheMoon/Models/Products.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq.Mapping;
+using System.Web;
+
+namespace ManOnTheMoon.Models
+{
+    [Table(Name ="Products")]
+    public class Products
+    {
+
+        [Column(IsPrimaryKey =true)]
+        int Id { get; set; }
+        [Column]
+        string Name { get; set; } 
+        [Column]
+        string Description { get; set; }
+        [Column]
+        int Upc { get; set; }
+        [Column]
+        string Brand { get; set; }
+        [Column]
+        float Length { get; set; }
+        [Column]
+        float Width { get; set; }
+        [Column]
+        float Height { get; set; }
+        [Column]
+        float Weight { get; set; }
+        [Column]
+        Decimal Cost { get; set; }
+        [Column]
+        Decimal Retail_Price { get; set; }
+        [Column]
+        Decimal Sale_Price { get; set; }
+        [Column]
+        int Stock_Quantity { get; set; }
+        [Column]
+        string Category { get; set; }
+        [Column]
+        string Sub_Category { get; set; }
+        [Column]
+        Boolean On_Sale_Status { get; set; }
+
+
+
+    }
+}
