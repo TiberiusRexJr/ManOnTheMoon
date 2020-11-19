8a6f27f34018a3fa4afd18b70ee9967bdbbd97c8
Lexus <rounin_lopez@yahoo.com>
2020-11-19 02:30:22 -0600

    completed current version of DbAdmin CRUD complete for all TAbles

--- ManOnTheMoon/Database/DbAdmin.cs
+++ ManOnTheMoon/Database/DbAdmin.cs
@@ -26,27 +26,26 @@ namespace ManOnTheMoon.Database
         #endregion
 
         #region Create
-        public bool CreateProduct(Product product)
+        public Nullable<int>  CreateProduct(Product product)
         {
-            bool status = false;
+            Nullable<int> id = null;
             if (product == null)
             {
-                return status;
+                return id;
             }
             try
             {
                 db.Products.InsertOnSubmit(product);
                 db.SubmitChanges();
-                status = true;
+                id = product.Id;
             }
             catch (Exception e)
             {
                 Errorhead(e);
 
 
-                status = false;
             }
-            return status;
+            return id;
         }
         public bool CreateCategory(Category category)
         {
@@ -91,13 +90,129 @@ namespace ManOnTheMoon.Database
             }
             return status;
         }
+        public bool CreateProductImageRecord(Product_Image product_Images)
+        {
+            bool status = false;
+            if(product_Images==null)
+            {
+                return status;
+            }
+            try
+            {
+                db.Product_Images.InsertOnSubmit(product_Images);
+                db.SubmitChanges();
+                status = true;
+            }
+            catch(Exception e)
+            {
+                Errorhead(e);
+            }
+            return status;
+        }
 
+        #endregion
+
+        #region Update
+            public bool UpdateProduct(Product product)
+            {
+            bool status = false;
+
+            if(product==null)
+            {
+                return status;
+            }
+            try
+            {
+                Product currentProduct = db.Products.Where(p => p.Id == product.Id).FirstOrDefault();
+                currentProduct.Name = product.Name;
+                currentProduct.Description = product.Description;
+                currentProduct.Upc = product.Upc;
+                currentProduct.Brand = product.Brand;
+                currentProduct.Length = product.Length;
+                currentProduct.Weight = product.Weight;
+                currentProduct.Height = product.Height;
+                currentProduct.Cost = product.Cost;
+                currentProduct.Retail_Price = product.Retail_Price;
+                currentProduct.Sale_Price = product.Sale_Price;
+                currentProduct.Stock_Quantity = product.Stock_Quantity;
+                currentProduct.Category = product.Category;
+                currentProduct.Sub_Category = product.Sub_Category;
+                currentProduct.On_Sale_Status = product.On_Sale_Status;
+                currentProduct.Width = product.Width;
+                db.SubmitChanges();
+            }
+            catch(Exception e)
+            {
+                Errorhead(e);
+            }
+            return status;
+            }
+            public bool UpdateCategory(Category category)
+            {
+            bool status = false;
+            if(category==null)
+            { return status; }
+            try
+            {
+
+                var oldCategory = db.Categories.Where(c => c.Id == category.Id).FirstOrDefault();
+                oldCategory.Name = category.Name;
+                db.SubmitChanges();
+                status = true;
+
+            }
+            catch(Exception e)
+            {
+                Errorhead(e);
+            }
+            return status;
+            }
+            public bool UpdateBrand(Brand brand)
+            {
+                bool status = false;
+                if(brand==null)
+
+                { return status; }
+            try
+            {
+                Brand oldBrand = db.Brands.Where(b => b.Id == brand.Id).FirstOrDefault();
+                oldBrand.Name = brand.Name;
+
+                db.SubmitChanges();
+                status = true;
+            }
+            catch(Exception e)
+            {
+                Errorhead(e);
+            }
+            return status;
+            }
+            public bool UpdateProductImages(Product_Image product_Images)
+            {
+                bool status = false;
+                try
+                {
+                Product_Image currentImageRecord = db.Product_Images.Where(p => p.Id == product_Images.Id).FirstOrDefault();
+                currentImageRecord.Product_Id = product_Images.Product_Id;
+                currentImageRecord.Product_Image_1 = product_Images.Product_Image_1;
+                currentImageRecord.Product_Image_2 = product_Images.Product_Image_2;
+                currentImageRecord.Product_Image_3 = product_Images.Product_Image_3;
+                db.SubmitChanges();
+                status = true;
+                }
+                catch(Exception e)
+                {
+                Errorhead(e);
+                }
+            return status;
+            }
+        
         #endregion
 
         #region RetrieveBy
         public Product productsById(int id)
         {
-            Product product = new Product() ;
+            Product product = null;
             try
             {
                 product = db.Products.Where(p => p.Id == id).FirstOrDefault();
@@ -111,7 +226,7 @@ namespace ManOnTheMoon.Database
         #endregion
 
         #region Delete
-            public bool DeleteById(Product p)
+            public bool DeleteProduct(Product p)
             {
                 bool status = false;
 
@@ -130,6 +245,57 @@ namespace ManOnTheMoon.Database
                 {
                 Errorhead(e);
                 }
+            return status;
+            }
+            public bool DeleteBrand(Brand brand)
+            {
+            bool status = false;
+            if(brand==null)
+            { return status; }
+            try
+            {
+                Brand oldBrand = db.Brands.Where(b => b.Id == brand.Id).FirstOrDefault();
+
+                status = true;
+            }
+            catch(Exception e)
+            {
+                Errorhead(e);
+            }
+            return status;
+            }
+            public bool DeleteCategory(Category category)
+            {
+            bool status = false;
+            if(category==null)
+            {
+                return status;
+            }
+            try
+            {
+                db.Categories.DeleteOnSubmit(category);
+                db.SubmitChanges();
+                status = true;
+            }
+            catch(Exception e)
+            {
+                Errorhead(e);
+            }
+            return status;
+            }
+            public bool DeleteAllProductImages(Product_Image product_Images)
+            {
+            bool status = false;
+            try
+            {
+                db.Product_Images.DeleteOnSubmit(product_Images);
+                db.SubmitChanges();
+                status = true;
+            }
+            catch (Exception e)
+            {
+                Errorhead(e);
+            }
             return status;
             }
         #endregion
