574bcebdaad26ca5a810195af87edf08292cf244
IronHead <rouninlopez@gmail.com>
2020-11-22 08:37:09 -0600

    fixed IImplmentation

--- ManOnTheMoon/Api/AdminController.cs
+++ ManOnTheMoon/Api/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using ManOnTheMoon.Models;
@@ -26,7 +27,8 @@ namespace ManOnTheMoon.Api
         {
             #region Properties
             public TReturnData returnData { get; set; }
-            public HttpRequestMessage requestMessage { get; set; }
+            public HttpStatusCode status { get; set; }
+            public string ReasonPhrase { get; set; }
             #endregion
 
             #region Constructor
@@ -35,18 +37,24 @@ namespace ManOnTheMoon.Api
 
             }
             #endregion
+
+            #region IImplementation
             public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
             {
-                
-                var data =
+
+                var data = JsonSerializer.Serialize(returnData);
                   var response = new HttpResponseMessage()
                   {
-                    Content = new StringContent(stringPayload, Encoding.UTF8, "application/json"),
-                    RequestMessage = _request
+                    Content = new StringContent(data,           Encoding.UTF8, "application/json"),
+                    StatusCode = status,
+                    ReasonPhrase=ReasonPhrase
+
+                    
                   };
 
                 return Task.FromResult(response);
             }
+            #endregion
         }
         #endregion
         #region Post
